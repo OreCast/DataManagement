cf64d11bbad22af407a5455b602be97634707ffd
Valentin Kuznetsov <vkuznet@gmail.com>
2023-09-19 11:18:06 -0400

    code refactoring

--- sites.go
+++ sites.go
@@ -19,32 +19,6 @@ type Site struct {
 	Description string `json:"description"`
 }
 
-// helper function to fetch sites info from discovery service
-func sites() []Site {
-	var out []Site
-	rurl := fmt.Sprintf("%s/sites", Config.DiscoveryURL)
-	if Config.Verbose > 0 {
-		log.Println("query Discovery service", rurl)
-	}
-	resp, err := http.Get(rurl)
-	if err != nil {
-		log.Println("ERROR:", err)
-		return out
-	}
-	defer resp.Body.Close()
-	var results []Site
-	dec := json.NewDecoder(resp.Body)
-	if err := dec.Decode(&results); err != nil {
-		log.Println("ERROR:", err)
-		return out
-	}
-	return results
-}
-
-// type BucketObject struct {
-//     Name string `json:"name"`
-// }
-
 // SiteObject represents site object
 type SiteObject struct {
 	Site    string             `json:"site"`
@@ -66,6 +40,28 @@ type DiscoveryRecord struct {
 	AccessKey    string `json:"access_key"`
 	AccessSecret string `json:"access_secret"`
 	UseSSL       bool   `json:"use_ssl"`
+}
+
+// helper function to fetch sites info from discovery service
+func sites() []Site {
+	var out []Site
+	rurl := fmt.Sprintf("%s/sites", Config.DiscoveryURL)
+	if Config.Verbose > 0 {
+		log.Println("query Discovery service", rurl)
+	}
+	resp, err := http.Get(rurl)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return out
+	}
+	defer resp.Body.Close()
+	var results []Site
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&results); err != nil {
+		log.Println("ERROR:", err)
+		return out
+	}
+	return results
 }
 
 // helper function to return S3 object for given site
@@ -103,10 +99,6 @@ func site2s3(site string) (S3, error) {
 	for _, rec := range records {
 		if rec.Name == site {
 			log.Printf("INFO: found %s in DataDiscovery records, will access its s3 via %s", rec.Name, rec.URL)
-			// bingo: we got desired site, now we can query its s3 storage for datasets
-			if Config.Verbose > 0 {
-				log.Println("###", rec.AccessKey, Config.DiscoveryPassword, Config.DiscoveryCipher)
-			}
 			akey, err := cryptoutils.HexDecrypt(rec.AccessKey, Config.DiscoveryPassword, Config.DiscoveryCipher)
 			if err != nil {
 				log.Printf("ERROR: unable to decrypt data discovery access key, error %v", err)
@@ -133,149 +125,54 @@ func site2s3(site string) (S3, error) {
 
 // siteContent provides content on given site
 func siteContent(site string) (SiteObject, error) {
-	if Config.Verbose > 0 {
-		log.Printf("looking for site:%s", site)
-	}
-	surl := fmt.Sprintf("%s/sites", Config.DiscoveryURL)
-	if Config.Verbose > 0 {
-		log.Println("query Discovery service", surl)
-	}
-	resp, err := http.Get(surl)
 	var siteObj SiteObject
+	s3, err := site2s3(site)
 	if err != nil {
-		log.Printf("ERROR: unable to contact DataDiscovery service %s, error %v", surl, err)
-		return siteObj, err
-	}
-	// read data discovery content
-	var records []DiscoveryRecord
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("ERROR: unable to read DataDiscovery response, error %v", err)
-		return siteObj, err
-	}
-	err = json.Unmarshal(body, &records)
-	if err != nil {
-		log.Printf("ERROR: unable to unmarshal DataDiscovery response, error %v", err)
 		return siteObj, err
 	}
 	if Config.Verbose > 0 {
-		log.Printf("site records %+v", records)
+		log.Printf("Use %v", s3)
 	}
-
-	for _, rec := range records {
-		if rec.Name == site {
-			log.Printf("INFO: found %s in DataDiscovery records, will access its s3 via %s", rec.Name, rec.URL)
-			// bingo: we got desired site, now we can query its s3 storage for datasets
-			if Config.Verbose > 0 {
-				log.Println("###", rec.AccessKey, Config.DiscoveryPassword, Config.DiscoveryCipher)
-			}
-			akey, err := cryptoutils.HexDecrypt(rec.AccessKey, Config.DiscoveryPassword, Config.DiscoveryCipher)
-			if err != nil {
-				log.Printf("ERROR: unable to decrypt data discovery access key, error %v", err)
-				return siteObj, err
-
-			}
-			apwd, err := cryptoutils.HexDecrypt(rec.AccessSecret, Config.DiscoveryPassword, Config.DiscoveryCipher)
-			if err != nil {
-				log.Printf("ERROR: unable to decrypt data discovery acess secret, error %v", err)
-				return siteObj, err
-
-			}
-			s3 := S3{
-				Endpoint:     rec.Endpoint,
-				AccessKey:    string(akey),
-				AccessSecret: string(apwd),
-				UseSSL:       rec.UseSSL,
-			}
-			if Config.Verbose > 0 {
-				log.Printf("INFO: accessing %+v", s3)
-			}
-			buckets, err := listBuckets(s3)
-			if err != nil {
-				log.Printf("ERROR: unabel to list buckets at %s, error %v", site, err)
-			}
-			obj := SiteObject{
-				Site:    site,
-				Buckets: buckets,
-			}
-			return obj, nil
-		}
+	log.Printf("INFO: found %s in DataDiscovery s3ords, will access its s3", site)
+	if Config.Verbose > 0 {
+		log.Printf("INFO: accessing %+v", s3)
 	}
-	return siteObj, errors.New("Not reachable")
+	buckets, err := listBuckets(s3)
+	if err != nil {
+		log.Printf("ERROR: unabel to list buckets at %s, error %v", site, err)
+	}
+	obj := SiteObject{
+		Site:    site,
+		Buckets: buckets,
+	}
+	return obj, nil
 }
 
 // bucketContent provides content on given site and bucket
 func bucketContent(site, bucket string) (BucketObject, error) {
+	var bucketObj BucketObject
+	s3, err := site2s3(site)
+	if err != nil {
+		return bucketObj, err
+	}
+	if Config.Verbose > 0 {
+		log.Printf("Use %v", s3)
+	}
 	if Config.Verbose > 0 {
 		log.Printf("looking for site:%s bucket:%s", site, bucket)
 	}
-	surl := fmt.Sprintf("%s/sites", Config.DiscoveryURL)
+	log.Printf("INFO: found %s in DataDiscovery records, will access its s3", site)
 	if Config.Verbose > 0 {
-		log.Println("query Discovery service", surl)
+		log.Printf("INFO: accessing %+v", s3)
 	}
-	resp, err := http.Get(surl)
-	var bucketObj BucketObject
+	objects, err := listObjects(s3, bucket)
 	if err != nil {
-		log.Printf("ERROR: unable to contact DataDiscovery service %s, error %v", surl, err)
-		return bucketObj, err
+		log.Printf("ERROR: unabel to list objects at %s/%s, error %v", site, bucket, err)
 	}
-	// read data discovery content
-	var records []DiscoveryRecord
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("ERROR: unable to read DataDiscovery response, error %v", err)
-		return bucketObj, err
+	obj := BucketObject{
+		Site:    site,
+		Bucket:  bucket,
+		Objects: objects,
 	}
-	err = json.Unmarshal(body, &records)
-	if err != nil {
-		log.Printf("ERROR: unable to unmarshal DataDiscovery response, error %v", err)
-		return bucketObj, err
-	}
-	if Config.Verbose > 0 {
-		log.Printf("site records %+v", records)
-	}
-
-	for _, rec := range records {
-		if rec.Name == site {
-			log.Printf("INFO: found %s in DataDiscovery records, will access its s3 via %s", rec.Name, rec.URL)
-			// bingo: we got desired site, now we can query its s3 storage for datasets
-			if Config.Verbose > 0 {
-				log.Println("###", rec.AccessKey, Config.DiscoveryPassword, Config.DiscoveryCipher)
-			}
-			akey, err := cryptoutils.HexDecrypt(rec.AccessKey, Config.DiscoveryPassword, Config.DiscoveryCipher)
-			if err != nil {
-				log.Printf("ERROR: unable to decrypt data discovery access key, error %v", err)
-				return bucketObj, err
-
-			}
-			apwd, err := cryptoutils.HexDecrypt(rec.AccessSecret, Config.DiscoveryPassword, Config.DiscoveryCipher)
-			if err != nil {
-				log.Printf("ERROR: unable to decrypt data discovery acess secret, error %v", err)
-				return bucketObj, err
-
-			}
-			s3 := S3{
-				Endpoint:     rec.Endpoint,
-				AccessKey:    string(akey),
-				AccessSecret: string(apwd),
-				UseSSL:       rec.UseSSL,
-			}
-			if Config.Verbose > 0 {
-				log.Printf("INFO: accessing %+v", s3)
-			}
-			objects, err := listObjects(s3, bucket)
-			if err != nil {
-				log.Printf("ERROR: unabel to list objects at %s/%s, error %v", site, bucket, err)
-			}
-			obj := BucketObject{
-				Site:    site,
-				Bucket:  bucket,
-				Objects: objects,
-			}
-			return obj, nil
-		}
-	}
-	return bucketObj, errors.New("Not reacheable")
+	return obj, nil
 }
